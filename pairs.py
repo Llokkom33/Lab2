7c05f5ba51e4dd5ce3a29e07b3bbc66e01c64ecd
LLIokm303 <132297679+LLIokm303@users.noreply.github.com>
2023-06-03 21:12:34 +0100

    updete

--- pairs.py
+++ pairs.py
@@ -1,16 +1,46 @@
-def find_pairs(numbers):
-    pairs = []
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] + numbers[j] == 10:
-                pairs.append((numbers[i], numbers[j]))
-    return pairs
+def analyze_file(file_path):
+    line_count = 0
+    empty_line_count = 0
+    lines_with_z_count = 0
+    total_z_count = 0
+    lines_with_and_count = 0
 
-# Задана колекція чисел
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    with open(file_path, 'r') as file:
+        for line in file:
+            line_count += 1
+            line = line.strip()
 
-pairs = find_pairs(numbers)
+            if line == "":
+                empty_line_count += 1
 
-# Виведення знайдених пар чисел
-for pair in pairs:
-    print(pair)
+            if "z" in line:
+                lines_with_z_count += 1
+                total_z_count += line.count("z")
+
+            if "and" in line:
+                lines_with_and_count += 1
+
+    print("Statistics for file:", file_path)
+    print("1. Line count:", line_count)
+    print("2. Empty line count:", empty_line_count)
+    print("3. Lines with 'z':", lines_with_z_count)
+    print("4. Total 'z' count:", total_z_count)
+    print("5. Lines with 'and':", lines_with_and_count)
+
+
+def main():
+    while True:
+        file_path = input("Enter the path to the file (or 'q' to quit): ")
+
+        if file_path.lower() == 'q':
+            break
+
+        analyze_file(file_path)
+
+        choice = input("Do you want to analyze another file? (y/n): ")
+        if choice.lower() != 'y':
+            break
+
+
+if __name__ == "__main__":
+    main()
--- readme.txt
+++ readme.txt
@@ -1,9 +1,21 @@
-Lab3 
+lab 4
 
-Результат коду
-(1, 9)
-(2, 8)
-(3, 7)
-(4, 6)
+Результат коду:
 
-Process finished with exit code 0
+Enter the path to the file (or 'q' to quit): example.txt
+Statistics for file: example.txt
+1. Line count: 5
+2. Empty line count: 1
+3. Lines with 'z': 2
+4. Total 'z' count: 3
+5. Lines with 'and': 1
+Do you want to analyze another file? (y/n): y
+Enter the path to the file (or 'q' to quit): another_example.txt
+Statistics for file: another_example.txt
+1. Line count: 4
+2. Empty line count: 0
+3. Lines with 'z': 1
+4. Total 'z' count: 1
+5. Lines with 'and': 2
+Do you want to analyze another file? (y/n): n
+ 
