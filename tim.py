dea0b1f91b3fc5e62563d188e4c586c48d607c83
Llokkom33 <135435212+Llokkom33@users.noreply.github.com>
2023-06-03 20:13:15 +0100

    Update tim.py

---
+++ tim.py
@@ -0,0 +1,10 @@
+Welcome!
+Enter a number (or 'q' to quit): 5
+five
+Enter a number (or 'q' to quit): 9
+nine
+Enter a number (or 'q' to quit): 12
+Invalid number
+Invalid input. Do you want to quit? (y/n): n
+Enter a number (or 'q' to quit): q
+Goodbye!
