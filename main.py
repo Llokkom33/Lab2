71bc720c05fd4b580be5db9f13de0e28b8d56a0e
LLIokm303 <132297679+LLIokm303@users.noreply.github.com>
2023-06-03 22:07:28 +0100

    updete

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-def number_to_word(number):
-    words = {
-        0: "zero", 1: "one", 2: "two", 3: "three", 4: "four",
-        5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"
-    }
-    return words.get(number, "Invalid number")
+import math
+
+class Task:
+    def __init__(self, a, m, b):
+        self.a = a
+        self.m = m
+        self.b = b
+        self.e = (a + 4*m + b) / 6
+        self.sd = (b - a) / 6
 
 def main():
-    print("Welcome!")
+    tasks = []
     while True:
-        try:
-            number = int(input("Enter a number (or 'q' to quit): "))
-            print(number_to_word(number))
-        except ValueError:
-            choice = input("Invalid input. Do you want to quit? (y/n): ")
-            if choice.lower() == 'y':
-                break
+        a = input("Enter the optimistic estimate for a task (or 'done' to finish): ")
+        if a == 'done':
+            break
+        m = float(input("Enter the most likely estimate for the task: "))
+        b = float(input("Enter the pessimistic estimate for the task: "))
+        tasks.append(Task(float(a), m, b))
 
-    print("Goodbye!")
+    total_e = sum(task.e for task in tasks)
+    total_sd = math.sqrt(sum(task.sd**2 for task in tasks))
 
-if __name__ == "__main__":
+    project_e = total_e
+    project_sd = total_sd
+
+    ci_min = project_e - 2*project_sd
+    ci_max = project_e + 2*project_sd
+
+    print(f"Project's 95% confidence interval: {ci_min:.2f} ... {ci_max:.2f} points")
+
+if __name__ == '__main__':
     main()
