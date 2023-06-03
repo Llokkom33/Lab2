e4fb5711a280b472ad31a3ffde4b4d973f59271c
Llokkom33 <135435212+Llokkom33@users.noreply.github.com>
2023-06-03 18:50:22 +0100

    Update tim.py

--- tim.py
+++ tim.py
@@ -1,15 +1,7 @@
-def find_pairs(numbers):
-    pairs = []
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] + numbers[j] == 10:
-                pairs.append((numbers[i], numbers[j]))
-    return pairs
+#Результат коду завдання
+(1, 9)
+(2, 8)
+(3, 7)
+(4, 6)
 
-# Задана колекція чисел
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-# Знаходження та друк пар чисел, сума яких дорівнює 10
-result = find_pairs(numbers)
-for pair in result:
-    print(pair)
+Process finished with exit code 0
