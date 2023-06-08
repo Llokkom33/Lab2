46a4f48088773fe768160a331e6a56114bd653db
LLIokm303 <132297679+LLIokm303@users.noreply.github.com>
2023-06-08 14:45:55 +0100

    first change

--- tim.py
+++
@@ -1,8 +0,0 @@
-Enter the optimistic estimate for a task (or 'done' to finish): 4
-Enter the most likely estimate for the task: 6
-Enter the pessimistic estimate for the task: 8
-Enter the optimistic estimate for a task (or 'done' to finish): 3
-Enter the most likely estimate for the task: 5
-Enter the pessimistic estimate for the task: 7
-Enter the optimistic estimate for a task (or 'done' to finish): done
-Project's 95% confidence interval: 4.57 ... 7.43 points
